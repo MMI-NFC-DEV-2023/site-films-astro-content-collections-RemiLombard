7f65f7243e6161cca5c89af3c2428bf0f63f4007
Rémi-Lombard <remi.lombard@edu.univ-fcomte.fr>
2025-02-11 11:34:34 +0100

    ajouter la prise en charge de @tailwindcss/typography, mettre à jour le formatage des dates et améliorer la mise en page des fiches de personnes

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,10 +2,6 @@ const formatter = new Intl.DateTimeFormat("fr-FR", {
     month: "long",
     day: "numeric",
     year: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    hour12: false,
-    timeZone: "Europe/Paris",
 });
 
 export function formatDate(date: Date) {
